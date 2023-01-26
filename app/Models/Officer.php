f6cee5d8ce7970cf7de7f06aad4b5781bb19000d
bc160200569 <118533654+bc160200569@users.noreply.github.com>
2023-01-26 19:00:35 +0500

    using Accessor and Mutators

--- app/Models/Officer.php
+++ app/Models/Officer.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -12,5 +13,26 @@ class Officer extends Model
     // protected $fillable = [
     //     'first_name','serial_no','batch_no','middle_name','last_name',
     // ];
+
+    // Using Mutator
+    public function setFirstNameAttribute($data){
+        $this->attributes['first_name'] = ucwords($data);
+    }
+    public function setLastNameAttribute($data){
+        $this->attributes['last_name'] = ucwords($data);
+    }
+    public function setMiddleNameAttribute($data){
+        $this->attributes['middle_name'] = ucwords($data.' name');
+    }
+    
+    // Using Accessor
+
+    public function getFirstNameAttribute($data){
+        return strtoupper($data);
+    }
+
+    public function getCreatedAtAttribute($data){
+        return Carbon::parse($this->attributes['created_at'])->deffForHumans($data);
+    }
 
 }
