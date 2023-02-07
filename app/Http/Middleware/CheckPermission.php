64cf89d69d43d319042de42111022a1d25e799cc
bc160200569 <118533654+bc160200569@users.noreply.github.com>
2023-02-07 18:07:26 +0500

    Check Permission Middelware Making for check routs

--- app/Http/Middleware/CheckPermission.php
+++ app/Http/Middleware/CheckPermission.php
@@ -2,10 +2,14 @@
 
 namespace App\Http\Middleware;
 
+use App\Models\User;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Spatie\Permission\Traits\HasPermissions;
+use Spatie\Permission\Models\Permission;
+
+
 
 class CheckPermission
 {
@@ -18,8 +22,28 @@ class CheckPermission
      */
     public function handle(Request $request, Closure $next)
     {
-        $role_name = Auth::user()->roles->pluck('id');
+        $routeName = $request->route()->getName();
+        $user = User::find(Auth::user()->id)->getPermissions();
+        $perm = json_decode($user, true);
+        $permission = $perm['permissions'];
+        // echo $routeName;
+        // dd($perm['permissions']);
+        if (in_array($routeName, $permission))
+        {
+            return $next($request);
+        // dd('yes');
+        }
+        else
+        {
+        // dd('no');
+        return redirect(route('home'))->withErrors(['AuthError' => 'You does not have the permissions to access. Please contact administrator for permission.']);
+        }
+        // $role_name = Auth::user()->roles->pluck('id');
+        // $per = Auth::user()->getallPermissions()->pluck('id');
+        // $permissions = $role_name->permissions->pluck('name');
         // $permission = $role_name->getAllPermissions();
+        // $routeName = $request->route()->getName();
+        // echo $role_name;
         // dd($permission);
         return $next($request);
     }
