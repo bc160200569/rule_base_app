fc7d5e489bfea23707c40220f0259f97761f6187
bc160200569 <118533654+bc160200569@users.noreply.github.com>
2023-02-07 18:19:20 +0500

    Error message hide after 2 seconds, redirect home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,19 @@
 @extends('layouts.app')
 
 @section('content')
+@if (count($errors) > 0)
+<div id="error" class="alert alert-danger">
+    <strong>Whoops!</strong> There were some problems with your input.<br><br>
+    <ul>
+        @foreach ($errors->all() as $error)
+        <li>{{ $error }}</li>
+        @endforeach
+    </ul>
+</div>
+@endif
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-12">
             <div class="card">
                 <div class="card-header">{{ __('Dashboard') }}</div>
 
@@ -14,10 +24,21 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    {{ __('Welcome to Roles and Permissions App!') }}
                 </div>
             </div>
         </div>
     </div>
 </div>
 @endsection
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script>
+    // $("#error").show();
+    // setTimeout(function() {
+    //     $("#error").hide();
+    // }, 5000);
+    $(function() {
+setTimeout(function() { $("#error").fadeOut(1500); }, 2000)
+
+})
+</script>
